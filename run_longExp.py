afea371a7f1ff3f2e080f2e6913fdb4eb45dd58d
Mateusz Drefs <mdrefs@gmx.de>
2024-08-22 13:05:07 +0200

    test

--- run_longExp.py
+++ run_longExp.py
@@ -42,7 +42,7 @@ def objective(trial):
     # Sparse-VQ
     parser.add_argument('--wFFN', type=int, default=1, help='use FFN layer')
     parser.add_argument('--svq', type=int, default=1, help='use sparse vector quantized')
-    parser.add_argument('--codebook_size', type=int, default=trial.suggest_int('codebook_size', 64, 256), help='codebook_size in sparse vector quantized')
+    parser.add_argument('--codebook_size', type=int, default=trial.suggest_int('codebook_size', [250, 500]), help='codebook_size in sparse vector quantized')
     
     parser.add_argument('--fc_dropout', type=float, default=0.05, help='fully connected dropout')
     parser.add_argument('--head_dropout', type=float, default=0.0, help='head dropout')
@@ -84,7 +84,8 @@ def objective(trial):
     parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
     parser.add_argument('--batch_size', type=int, default=128, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=5, help='early stopping patience')
-    parser.add_argument('--learning_rate', type=float, default=trial.suggest_loguniform('learning_rate', 1e-5, 1e-3), help='optimizer learning rate')
+    #parser.add_argument('--learning_rate', type=float, default=trial.suggest_loguniform('learning_rate', 1e-5, 1e-3), help='optimizer learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
     parser.add_argument('--des', type=str, default='test', help='exp description')
     parser.add_argument('--loss', type=str, default='mae', help='loss function')
     parser.add_argument('--lradj', type=str, default='TST', help='adjust learning rate')
